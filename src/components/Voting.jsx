23b245104ee582a64e51be69454ad120dc438a69
Jeremy Meyer <jjman505@gmail.com>
2016-01-16 16:47:48 -0500

    refact: extract Winner and Vote from Voting

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
-  isDisabled: function() {
-    return !!this.props.hasVoted;
-  },
-  getPair: function() {
-    return this.props.pair || [];
-  },
-  hasVotedFor: function(entry) {
-    return this.props.hasVoted === entry;
-  },
   render: function() {
-    return (
-      <div className="voting">
+    return <div>
       {this.props.winner ?
-        <div ref="winner">Winner is {this.props.winner}!</div> :
-        this.getPair().map(entry =>
-          <button key={entry}
-                  disabled={this.isDisabled()}
-                  onClick={() => this.props.vote(entry)}>
-            <h1>{entry}</h1>
-            {this.hasVotedFor(entry) ?
-              <div className="label">Voted</div> :
-              null}
-          </button>
-        )}
-      </div>
-    );
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />}
+    </div>;
   }
 });
