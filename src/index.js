f68041471d977e2c26e8a8373e1fc98c69802df1
Jeremy Meyer <jjman505@gmail.com>
2016-01-16 18:03:22 -0500

    feat: add react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
+import {Route} from 'react-router';
+import App from './components/App';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
+
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting" />,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
